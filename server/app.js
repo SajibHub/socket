860cbd662c40630032370b596e832efeedbc304d
SajibHub <mohammadsajib996@gmail.com>
2025-03-24 10:44:11 +0600

    update

--- server/app.js
+++ server/app.js
@@ -16,53 +16,55 @@ app.use(express.urlencoded({ extended: true }));
 const io = new Server(server, {
     cors: {
         origin: "http://localhost:5173",
-        credentials: true 
+        credentials: true
     }
 });
 
 let users = {};
 
 const updateUsers = () => {
-    Object.keys(users).forEach((userId) => {
-        const userList = Object.keys(users).filter(id => id != userId);
-        io.to(userId).emit("user_list", userList);
-    });
+    io.emit("user_list", Object.keys(users));
 };
 
-
 io.on('connection', (socket) => {
-    const userId = socket.handshake.query.id;
-    if (userId) {
-        users[userId] = socket.id;
-        updateUsers(userId); 
-    }
-
-    socket.on('private_message', (data) => {
-        const { recipientId, message } = data;
-        const recipientSocketId = users[recipientId];
-      
-        if (recipientSocketId) {
-          io.to(recipientSocketId).emit('private_message', {
-            senderId: userId,
-            message: message,
-            status: "delivered",
-          });
-        } else {
-          console.log(`User ${recipientId} not found`);
+    // When a user connects, they need to join with a userId
+    socket.on('join', (userId) => {
+        if (userId) {
+            // Store the user with their socket id
+            users[userId] = socket.id;
+            // Join a room specific to this user
+            socket.join(userId);
+            console.log(`User ${userId} joined`);
+            updateUsers();
+            
+            // Send current user list to the newly connected user
+            socket.emit("user_list", Object.keys(users).filter(id => id !== userId));
         }
     });
-      
+
+    socket.on('private_message', (data) => {
+        const { recipientId, message, senderId } = JSON.parse(data);
+        
+        // Send message to the recipient's room
+        if (recipientId && users[recipientId]) {
+            io.to(recipientId).emit('private_message', {
+                senderId: senderId,
+                message: message,
+                status: "delivered",
+            });
+        }
+    });
+
     socket.on('disconnect', () => {
+        // Find and remove the user from the users object
+        const userId = Object.keys(users).find(id => users[id] === socket.id);
         if (userId) {
             delete users[userId];
             console.log(`User disconnected: ${userId}`);
-            updateUsers(); 
+            updateUsers();
         }
     });
-
-    socket.emit("user_list", Object.keys(users).filter(id => id !== userId));
 });
-
 
 server.listen(3030, () => {
     console.log("Server is running on port 3030");
